392c03c1298fbcbce74c13e8b51b61d5b6fb6328
Bhavana130 <76762928+Bhavana130@users.noreply.github.com>
2021-08-29 10:03:50 +0530

    Update Activity_12.py

---
+++ Activity_12.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter second number: "))
+num3=int(input("Enter the third number: "))
+
+if(num1>num2) and (num1>num3):
+    biggest=num1;
+
+elif (num2>num1) and (num2>num3):
+    biggest=num2;
+
+else:
+    biggest=num3;
+print("the biggest number is",biggest);
+
